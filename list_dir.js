70a752b1fa0e4b8259a2686130d7828ce9713a3b
Ashwin Mridul <ashwinmridul@Ashwins-MacBook-Pro.local>
2017-09-28 22:41:37 +0530

    Read directories synchronously

--- list_dir.js
+++ list_dir.js
@@ -2,22 +2,22 @@ const fs = require("fs"),
     path = require("path");
 
 function listDir(currentDirPath, callback) {
-    fs.readdir(currentDirPath, function (err, files) {
-        if (err) {
-            callback(err, []);
-        } else {
-            var returnList = [];
-            files.forEach(function (name) {
-                var filePath = path.join(currentDirPath, name);
-                var stat = fs.statSync(filePath);
-                if (stat.isFile()) {
-                    returnList.push({type: "file", name: name, size: 4096});
-                } else if (stat.isDirectory()) {
-                    returnList.push({type: "folder", name: name, size: 0});
-                }
-            });
-            callback(null, returnList);
-        }
-    });
+    try {
+        var files = fs.readdirSync(currentDirPath);
+        var returnList = [];
+        files.forEach(function (name) {
+            var filePath = path.join(currentDirPath, name);
+            var stat = fs.statSync(filePath);
+            if (stat.isFile()) {
+                returnList.push({type: "file", name: name, size: 4096});
+            } else if (stat.isDirectory()) {
+                returnList.push({type: "folder", name: name, size: 0});
+            }
+        });
+        callback(null, returnList);
+    } catch (e) {
+        callback(e, []);
+    }
+
 };
 module.exports = listDir;
